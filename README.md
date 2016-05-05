e5bcde440f1c7ce6f65a52c86f8d34f21efaad78
Dylan Held <sgonew5@gmail.com>
2016-05-04 23:33:59 -0400

    Updating coding style and readme

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ ASCII-based chess game completed with partner, Andrew Clappison, in early Decemb
 
 # Environment
 Platform: Windows (8.1 tested)
+
+# Getting Started
+To begin a game input "game [human/computer{1,2,3,4}] [human/computer{1,2,3,4}]", e.x. "game human human" starts a game with two human players
+
+# Restrictions
+Once a game has begun, one must complete entire game before inputting "exit" releases program
--- src/human.cc
+++ src/human.cc
@@ -15,85 +15,78 @@ Human::Human(GameBoard* gb, int colour): Player(gb, colour) {}
 //        (2) Checks that end location is a avaliable move for the piece 
 //        (3) Calls Method that proposed move wont put their own king in check
 //        (4) Above methods indirectly confirm starting and ending locations are on board
-void Human::makeMove() {
-  bool validRequest = false;
-  while(!validRequest) {
-    bool makeTheMove = false;
-    string cmdLine;
-    string cmd_type;
-    Coord stc, endc;
-
-    getline(cin, cmdLine);
-    if (cmdLine == "") getline(cin, cmdLine);
-    istringstream cmd(cmdLine);
-    cmd >> cmd_type;
-   
-    //This code block preforms the 4 checks
-    if (cmd_type == "move") {
-	stc >> cmd;
-	//      in >> stc;
-	endc >> cmd;
-        //cmd >> endc;
-        #ifdef DEBUG
-	//cout << "Hello" << stc.getX() << " " << stc.getY() << endl;
-        //cout << endc.getX() << " " << endc.getY() << endl;
-       
-	//cout << gb->getPiece(stc)->getPiece() << endl;
-        //cout << "getcolor: " << gb->getPiece(stc)->getColour() << endl;
-        //cout << "color " << colour << endl;
-      	#endif
-	if(isMyPiece(gb->getPiece(stc))) {
-        vector <Coord> v;
-        v = gb->getPiece(stc)->potentialMoves(stc); //Change potentialMoves to return a vector
-      
-        for(vector<Coord>::iterator it = v.begin(); it != v.end(); ++it) {
-          #ifdef DEBUG
-	  //cout << "potential: " << it->getX() << " " << it->getY() << endl; 
-	  #endif
-	  bool same = ((it->getX() == endc.getX()) && (it->getY() == endc.getY()));
-          if (same && !(wouldPutInCheck(stc,endc))) makeTheMove = true;
-        }
-      }
-    }
-    // Add pawn promotion!
-    string apple;
-    bool canPP = false;
-    //cout << "got here" << endl;
-    if(makeTheMove && ((colour == 0 && endc.getY() == 7 && gb->getPiece(stc)->getPiece() == 'P') ||
-	(colour == 1 && endc.getY() == 0 && gb->getPiece(stc)->getPiece() == 'p'))) {
+void Human::makeMove()
+{
+	bool validRequest = false;
+	while(!validRequest) 
+	{
+		bool makeTheMove = false;
+		string cmdLine;
+		string cmd_type;
+		Coord stc, endc;
+		if (!getline(cin, cmdLine))
+			break;
+		//if (cmdLine == "") 
+		//	getline(cin, cmdLine);
+		istringstream cmd(cmdLine);
+		cmd >> cmd_type;
+		
+		//This code block preforms the 4 checks
+		if (cmd_type == "move") 
+		{
+			stc >> cmd;
+			endc >> cmd;
+			if(isMyPiece(gb->getPiece(stc))) 
+			{
+				vector <Coord> v;
+				v = gb->getPiece(stc)->potentialMoves(stc); //Change potentialMoves to return a vector
+				for(vector<Coord>::iterator it = v.begin(); it != v.end(); ++it) 
+				{
+					bool same = ((it->getX() == endc.getX()) && (it->getY() == endc.getY()));
+					if (same && !(wouldPutInCheck(stc,endc))) 
+						makeTheMove = true;
+				}
+			}
+		}
+		
+		// Pawn promotion
+		string promoteTo;
+		bool canPP = false;
+		if(makeTheMove && ((colour == 0 && endc.getY() == 7 && gb->getPiece(stc)->getPiece() == 'P') ||
+			(colour == 1 && endc.getY() == 0 && gb->getPiece(stc)->getPiece() == 'p'))) 
+		{
 #ifdef DEBUG
-		cout << "Pawn Promotion occured!" << endl;
+			cout << "Pawn Promotion occured!" << endl;
 #endif
-		bool whichState = false;
-		//canPP = false;
-		//char checkState = gb->getPiece(stc)->getPiece()
-		if(isupper(gb->getPiece(stc)->getPiece())) { whichState = true; }
-		cmd >> apple;
-			if(whichState && (apple == "Q" || apple == "R" || apple == "N" || apple == "B")) {
-				//makeTheMove = true;
-				canPP = true;	
-			}
-			else if(!whichState && (apple == "q" || apple == "r" || apple == "n" || apple == "b")) {
-                                //makeTheMove = true;
-                        	canPP = true;
-			}
-			else {
+			bool whichState = false;
+			if(isupper(gb->getPiece(stc)->getPiece())) 
+				whichState = true; 
+			cmd >> promoteTo;
+			if(whichState && (promoteTo == "Q" || promoteTo == "R" || promoteTo == "N" || promoteTo == "B"))
+				canPP = true;
+			else if(!whichState && (promoteTo == "q" || promoteTo == "r" || promoteTo == "n" || promoteTo == "b"))
+				canPP = true;
+			else 
+			{
 				makeTheMove = false;
 				cerr << "Invalid Promotion piece" << endl;
 			}
-	}
-    if (makeTheMove) {
-      gb->commitMove(stc, endc); //Need to add this function to our GameBoard Class
-      if(canPP) {
-		gb->promotePawn(endc, apple);
-      }
-      validRequest = true;
-    } else if (cmd_type == "resign") {
-      gb->setResigned(true);
-      validRequest = true;
-    } else {
-      cerr << "Invalid Request: no change occured." << endl;
-    }
-
-  } //Exit upon valid command read
+		}
+		if (makeTheMove) 
+		{
+			gb->commitMove(stc, endc); //Need to add this function to our GameBoard Class
+			if(canPP) 
+			{
+				gb->promotePawn(endc, promoteTo);
+			}
+			validRequest = true;
+		}
+		else if (cmd_type == "resign") 
+		{
+			gb->setResigned(true);
+			validRequest = true;
+		} 
+		else 
+			cerr << "Invalid Request: no change occured." << endl;
+	} //Exit upon valid command read
 }
